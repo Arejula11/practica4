c8d4d16f7e92b30c151413731f8bf63b1eb07fb0
Miguel Aréjula Aisa <92888725+Arejula11@users.noreply.github.com>
2022-05-17 12:00:44 +0200

    clase

--- costemsolape.cpp
+++ costemsolape.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include "maxsolape.hpp"
+#include <time.h>
+#include <cstdlib>
 
 using namespace std;
-void probarMax(double  inters[N][2], double inters2[N][2]){
+const int DERECHA = 100;
+const int IZQUIERDA = 4000;
 
-	tpSolape sol= maxSolFBruta(inters,5);
-	tpSolape sol2= maxSolFBruta(inters2,5);
+int numRandInter(){
+	time_t semilla = time(nullptr);
+	srand(semilla);
+	int numIntervalos;
+	numIntervalos= DERECHA+rand()%(IZQUIERDA-DERECHA+1);
+
+}
+
+void randomInter(double intervalos[N][2], const int numIntervalos){
+	time_t semilla = time(nullptr);
+	srand(semilla);
+	for (int i = 0; i < numIntervalos;i++){
+		intervalos[i][0] = minini+(maxfin-minini)*rand()/RAND_MAX;
+		intervalos[i][1] =  minini+(maxfin-minini)*rand()/RAND_MAX;
+		if(intervalos[i][0]>intervalos[i][1]){
+			double aux = intervalos[i][0];
+			intervalos[i][0] = intervalos[i][0];
+			intervalos[i][1] = aux;
+		}
+	}
 	
-	cout<<"Solape = "<<sol.solape<<" A = "<<sol.interA<<" B = "<<sol.interB<<endl;
-	cout<<"Solape = "<<sol2.solape<<" A = "<<sol2.interA<<" B = "<<sol2.interB<<endl;
+
 }
+
+
 int main(){
+		
+	double intervalos[N][2];
+	const int numIntervalos = numRandInter();
+	randomInter(intervalos, numIntervalos);
+	cout<<numIntervalos<<endl;
+	for(int i = 0; i<numIntervalos;i++){
 
-	double inters[N][2] = {
-        	{1.5, 8.0},
-        	{0.0, 4.5},
-        	{2.0, 4.0},
-      		{1.0, 6.0},
-	        {3.5, 7.0},
-}
+	cout <<i << ": " <<  intervalos[i][0] << " - " <<  intervalos[i][1]<< endl;
+	} 
+	
 
-	cout<<"PROBAR MAX:"<<endl;
-	probarMax(inters,inters2);
+	
+	return 0;
 }
